ddb518c9ed84a55faaf69e8b718b75e7e4af5859
isaacCodes1 <damizickola@gmail.com>
2024-06-11 11:54:44 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+fetch('https://fakestoreapi.com/products').then((myDataResponse)=>{
+  // console.log(myDataResponse.json);
+  return myDataResponse.json(); //converted Json format to an Object
+}).then((objectData)=>{
+  console.log(objectData[0].title);
+  let myTableData = "";
+  objectData.map((values)=>{
+    myTableData += `<tr>
+        <td>${values.title}</td>
+        <td>${values.description}</td>
+        <td>${values.price}</td>
+        <td><img src="${values.image}"/></td>
+      </tr>`;
+  });
+  document.getElementById('myTable_Body').innerHTML = myTableData;
+}).catch((error)=>{
+  console.log(error);
+})
